d2f4558f48f441c1208293d0d3467745f481dd0d
Ismail2830 <ismailaitrehail2830@gmail.com>
2023-12-29 16:26:03 +0100

    image Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/imge-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/imge-form.tsx
@@ -2,24 +2,14 @@
 
 import * as z from "zod"
 import axios from "axios"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
-import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormMessage,
-  } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react"
-import { Pencil } from "lucide-react"
-import { cn } from "@/lib/utils"
-import { Textarea } from "@/components/ui/textarea"
+import { ImageIcon, Pencil, PlusCircle } from "lucide-react"
 import { Course } from "@prisma/client"
+import Image from "next/image"
+import { FileUpload } from "@/components/file-upload"
 
 
 interface ImageFormProps{
@@ -41,14 +31,6 @@ export const ImageForm = ({
 
     const router = useRouter();
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            imageUrl: initialData?.imageUrl || ""
-        },
-      });
-      const { isSubmitting, isValid } = form.formState;
-
       const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
             await axios.patch(`/api/courses/${courseId}`, values);
@@ -65,9 +47,16 @@ export const ImageForm = ({
             <div className="font-medium flex items-center justify-between">
         Course image
         <Button onClick={toggleEdit} variant="ghost">
-          {isEditing ? (
+        {isEditing && (
             <>Cancel</>
-          ) : (
+          )}
+          {!isEditing && !initialData.imageUrl && (
+            <>
+              <PlusCircle className="h-4 w-4 mr-2" />
+              Add an image
+            </>
+          )}
+          {!isEditing && initialData.imageUrl && (
             <>
               <Pencil className="h-4 w-4 mr-2" />
               Edit image
@@ -76,45 +65,35 @@ export const ImageForm = ({
         </Button>
       </div>
       {!isEditing && (
-        <p className={cn(
-            "text-sm mt-2",
-            !initialData.imageUrl && "text-slate-500 italic"
-          )}>
-          {initialData.imageUrl || "No description"}
-        </p>
+        !initialData.imageUrl ? (
+          <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
+            <ImageIcon className="h-10 w-10 text-slate-500" />
+          </div>
+        ) : (
+          <div className="relative aspect-video mt-2">
+            <Image
+              alt="Upload"
+              fill
+              className="object-cover rounded-md"
+              src={initialData.imageUrl}
+            />
+          </div>
+        )
       )}
       {isEditing && (
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-4 mt-4"
-          >
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Textarea
-                      disabled={isSubmitting}
-                      placeholder="e.g. 'This course is about...'"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="flex items-center gap-x-2">
-              <Button
-                disabled={!isValid || isSubmitting}
-                type="submit"
-              >
-                Save
-              </Button>
-            </div>
-          </form>
-        </Form>
+        <div>
+        <FileUpload
+          endpoint="courseImage"
+          onChange={(url) => {
+            if (url) {
+              onSubmit({ imageUrl: url });
+            }
+          }}
+        />
+        <div className="text-xs text-muted-foreground mt-4">
+          16:9 aspect ratio recommended
+        </div>
+      </div>
       )}
         </div>
     )
